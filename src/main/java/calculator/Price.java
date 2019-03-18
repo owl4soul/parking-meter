4a9370cb316d40cbdb2a72e16efd5acb40415d3f
owl4soul <resuuser989@gmail.com>
2019-03-18 19:58:03 +0300

    0.4 fixed Instant + Formatter
    - Исправлены ошибки при парсинге даты за счет округления до HH:mm

--- src/main/java/calculator/Price.java
+++ src/main/java/calculator/Price.java
@@ -19,14 +19,27 @@ public class Price {
 
 
     public void applyPrice(LocalDateTime fromDate, LocalDateTime toDate) {
-        System.out.println(fromDate.toString());
+        System.out.println("from " + fromDate);
+        System.out.println("to " + toDate);
+
+        System.out.println();
+        //Round down to hours
+        LocalDateTime floorFromDate = fromDate.withMinute(0).withSecond(0).withNano(0);
+        System.out.println("Rounded down start date: " + floorFromDate);
+        //Round up to hours
+        LocalDateTime ceilToDate = toDate.plusHours(1).withMinute(0).withSecond(0).withNano(0);
+        System.out.println("Rounded up end date: " + ceilToDate);
+        System.out.println()
+        ;
         DateTimeFormatter formatter = DateTimeFormatter
-                .ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+                .ofPattern("yyyy-MM-dd'T'HH:mm")
                 .withZone(ZoneId.of("GMT"));
 //        System.out.println(ZoneId.getAvailableZoneIds());
-        Instant res = Instant.from(formatter.parse(fromDate.toString()));
-        System.out.println(res.toString());
 
+        Instant instantStart = Instant.from(formatter.parse(floorFromDate.toString()));
+        System.out.println("Зафиксированное время начала парковки: "+ instantStart.toString());
+        Instant instantEnd = Instant.from(formatter.parse(ceilToDate.toString()));
+        System.out.println("Зафиксированное время окончания парковки: "+ instantEnd.toString());
 
 
 //        LocalDateTime floorFromDate = LocalDateTime.
